438340d86afca9429ce792acfe8f590f7c70e335
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-08-30 18:02:34 +1000

    Add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+  },
+  "extends": [
+    "plugin:react/recommended",
+    "google",
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module",
+  },
+  "plugins": [
+    "react",
+    "react-hooks",
+  ],
+  "ignorePatterns": [
+    "src/*.css",
+  ],
+  "rules": {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "quotes": ["error", "double"],
+    "max-len": "off",
+    "semi": ["error", "always"],
+    "require-jsdoc": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  "settings": {
+    "react": {
+      "pragma": "React",
+      "version": "detect",
+    },
+  },
+};

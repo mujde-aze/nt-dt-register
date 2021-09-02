aebe2b34a354329afc827e13b33e30721650029c
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-09-02 13:44:28 +1000

    Handle change event for each form field

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -10,17 +10,18 @@ const loader = new Loader({
 });
 
 function RegistrationForm() {
-  const [givenName, setGivenName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [age, setAge] = useState("");
-  const [street, setStreet] = useState("");
-  const [flatNumber, setFlatNumber] = useState("");
-  const [streetNumber, setStreetNumber] = useState("");
-  const [city, setCity] = useState("");
-  const [area, setArea] = useState("");
-  const [country, setCountry] = useState("");
-  const [ageConfirmation, setAgeConfirmation] = useState("");
-
+  const [formState, setFormState] = useState({
+    givenName: "",
+    surname: "",
+    age: "",
+    street: "",
+    flatNumber: "",
+    streetNumber: "",
+    city: "",
+    area: "",
+    country: "",
+    ageConfirmation: "",
+  });
 
   useEffect(() => {
     loader.load()
@@ -32,44 +33,21 @@ function RegistrationForm() {
   }, []);
 
   function handleChange(event) {
-    switch (event.target.name) {
-      case "givenName":
-        setGivenName(event.target.value);
+    const newFormState = {...formState};
+    for (const property in newFormState) {
+      if (property === event.target.name) {
+        newFormState[property] = event.target.value;
         break;
-      case "surname":
-        setSurname(event.target.value);
-        break;
-      case "age":
-        setAge(event.target.value);
-        break;
-      case "street":
-        setStreet(event.target.value);
-        break;
-      case "flatNumber":
-        setFlatNumber(event.target.value);
-        break;
-      case "streetNumber":
-        setStreetNumber(event.target.value);
-        break;
-      case "city":
-        setCity(event.target.value);
-        break;
-      case "area":
-        setArea(event.target.value);
-        break;
-      case "country":
-        setCountry(event.target.value);
-        break;
-      case "ageConfirmation":
-        setAgeConfirmation(event.target.value);
-        break;
+      }
     }
+    setFormState(newFormState);
   }
 
   function handleSubmit(event) {
-    console.log(ageConfirmation);
-    console.log(age);
-    console.log(city);
+    console.log(formState.ageConfirmation);
+    console.log(formState.age);
+    console.log(formState.city);
+    console.log(formState.street);
     event.preventDefault();
   }
 
@@ -79,19 +57,21 @@ function RegistrationForm() {
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridGivenName">
             <Form.Label>Given Name</Form.Label>
-            <Form.Control type="text" name="givenName" placeholder="Enter given name" value={givenName} onChange={((event) => handleChange(event))}/>
+            <Form.Control type="text" name="givenName" placeholder="Enter given name"
+              value={formState.givenName} onChange={((event) => handleChange(event))}/>
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridSurname">
             <Form.Label>Surname</Form.Label>
-            <Form.Control type="text" name="surname" placeholder="Enter surname" value={surname} onChange={((event) => handleChange(event))}/>
+            <Form.Control type="text" name="surname" placeholder="Enter surname"
+              value={formState.surname} onChange={((event) => handleChange(event))}/>
           </Form.Group>
         </Row>
         <Row className="mb-3">
           <Col xs={4}>
             <Form.Group as={Col} controlId="formGridAge">
               <Form.Label>Age</Form.Label>
-              <Form.Select name="age" value={age} onChange={((event) => handleChange(event))}>
+              <Form.Select name="age" value={formState.age} onChange={((event) => handleChange(event))}>
                 <option>Select your age group</option>
                 <option>18-22</option>
                 <option>23-25</option>
@@ -106,20 +86,23 @@ function RegistrationForm() {
         <Row className="mb-3">
           <Form.Group as={Col} controlId="location">
             <Form.Label>Street Name</Form.Label>
-            <Form.Control type="text" name="street" placeholder="Enter street name" value={street} onChange={((event) => handleChange(event))}/>
+            <Form.Control type="text" name="street" placeholder="Enter street name"
+              value={formState.street} onChange={((event) => handleChange(event))}/>
           </Form.Group>
         </Row>
         <Row className="mb-3">
           <Col xs={4}>
             <Form.Group as={Col} controlId="formGridFlatNumber">
               <Form.Label>Flat Number</Form.Label>
-              <Form.Control type="text" name="flatNumber" placeholder="Enter flat number" value={flatNumber} onChange={((event) => handleChange(event))}/>
+              <Form.Control type="text" name="flatNumber" placeholder="Enter flat number"
+                value={formState.flatNumber} onChange={((event) => handleChange(event))}/>
             </Form.Group>
           </Col>
           <Col xs={4}>
             <Form.Group as={Col} controlId="street_number">
               <Form.Label>Street Number</Form.Label>
-              <Form.Control type="text" name="streetNumber" placeholder="Enter street number" value={streetNumber} onChange={((event) => handleChange(event))}/>
+              <Form.Control type="text" name="streetNumber" placeholder="Enter street number"
+                value={formState.streetNumber} onChange={((event) => handleChange(event))}/>
             </Form.Group>
           </Col>
         </Row>
@@ -127,13 +110,15 @@ function RegistrationForm() {
           <Col xs={4}>
             <Form.Group as={Col} controlId="locality">
               <Form.Label>City</Form.Label>
-              <Form.Control type="text" name="city" placeholder="Enter city" value={city} onChange={((event) => handleChange(event))}/>
+              <Form.Control type="text" name="city" placeholder="Enter city"
+                value={formState.city} onChange={((event) => handleChange(event))}/>
             </Form.Group>
           </Col>
           <Col xs={4}>
             <Form.Group as={Col} controlId="sublocality_level_1">
               <Form.Label>Area</Form.Label>
-              <Form.Control type="text" name="area" placeholder="Enter Area" value={area} onChange={((event) => handleChange(event))}/>
+              <Form.Control type="text" name="area" placeholder="Enter Area"
+                value={formState.area} onChange={((event) => handleChange(event))}/>
             </Form.Group>
           </Col>
         </Row>
@@ -141,14 +126,15 @@ function RegistrationForm() {
           <Col xs={4}>
             <Form.Group as={Col} controlId="country">
               <Form.Label>Country</Form.Label>
-              <Form.Control type="text" name="country" placeholder="Country" value={country} onChange={((event) => handleChange(event))}/>
+              <Form.Control type="text" name="country" placeholder="Country"
+                value={formState.country} onChange={((event) => handleChange(event))}/>
             </Form.Group>
           </Col>
         </Row>
         <Row className="mb-3">
           <Form.Group className="mb-3" id="formGridAgeConfirmation">
             <Form.Check type="checkbox" name="ageConfirmation" id="age-confirmation"
-              label="I am older than 18" value={ageConfirmation} onChange={((event) => handleChange(event))}/>
+              label="I am older than 18" value={formState.ageConfirmation} onChange={((event) => handleChange(event))}/>
           </Form.Group>
         </Row>
         <Row className="mb-3">

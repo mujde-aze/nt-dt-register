79e3077daa89cc0c4a71c9d18ac8fb4d66e8ce50
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-09-03 08:43:31 +1000

    Complete and clean but totally broken, too many rerenders

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,15 +1,10 @@
 import {Button, Col, Form, Row} from "react-bootstrap";
-import {useEffect, useRef, useState} from "react";
-import {Loader} from "@googlemaps/js-api-loader";
-
-const loader = new Loader({
-  apiKey: process.env.REACT_APP_API_KEY,
-  version: "weekly",
-  libraries: ["places"],
-});
+import {useRef, useState} from "react";
+import useGoogleAutoComplete from "../Hooks/GoogleAutoComplete";
 
 function RegistrationForm() {
   const streetElement = useRef();
+  const placeItems = useGoogleAutoComplete(streetElement);
   const [formState, setFormState] = useState({
     givenName: "",
     surname: "",
@@ -22,65 +17,18 @@ function RegistrationForm() {
     ageConfirmation: "",
   });
 
-  useEffect(() => {
-    loader.load()
-        .then((google) => {
-          const autocomplete = new google.maps.places.Autocomplete(streetElement.current, {
-            componentRestrictions: {country: ["aze"]},
-            fields: ["address_components", "geometry"],
-            types: ["address"],
-          });
-          autocomplete.addListener("place_changed", () => {
-            const place = autocomplete.getPlace();
-            fillInAddress(place);
-          });
-        }).catch((e) => {
-          console.error(`Failed to load autocomplete: ${e}`);
-        });
-  }, []);
-
-  function fillInAddress(place) {
-    const addressNameFormat = {
-      "street_number": "short_name",
-      "route": "long_name",
-      "locality": "long_name",
-      "sublocality_level_1": "short_name",
-      "country": "long_name",
-    };
-    const getAddressComp = function(type) {
-      for (const component of place.address_components) {
-        if (component.types[0] === type) {
-          return component[addressNameFormat[type]];
-        }
-      }
-      return "";
-    };
-
-    setFormState({...formState, street: getAddressComp("street_number") + " " + getAddressComp("route"),
-      city: getAddressComp("locality"),
-      area: getAddressComp("sublocality_level_1"),
-      country: getAddressComp("country"),
-    });
+  console.log(placeItems);
+  if (placeItems !== undefined) {
+    setFormState({...formState, ...placeItems});
   }
 
   function handleChange(event) {
-    const newFormState = {...formState};
-    for (const property in newFormState) {
-      if (property === event.target.name) {
-        newFormState[property] = event.target.value;
-        break;
-      }
-    }
-    setFormState(newFormState);
+    setFormState({...formState, [event.target.name]: event.target.value});
   }
 
   function handleSubmit(event) {
-    console.log(formState.ageConfirmation);
-    console.log(formState.age);
-    console.log(formState.city);
-    console.log(formState.street);
-    console.log(formState.givenName);
     event.preventDefault();
+    console.log(formState);
   }
 
   return (

104101c8b9afdc2f2ee6f7eaf3de3cbe1258fc40
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-09-11 11:05:57 +1000

    Add recaptcha to submit button

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.2] - 2021-09-11
+
+### Added
+- Submit button integration with Recaptcha. Appcheck doesn't automatically submit this token so we have to do it manually
+
 ## [0.4.1] - 2021-09-10
 
 ### Changed
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -35,8 +35,7 @@ function RegistrationForm({firebase}) {
     }
   }
 
-  async function handleSubmit(event) {
-    event.preventDefault();
+  async function handleSubmit(token) {
     const functions = getFunctions(firebase, "australia-southeast1");
     if (process.env.REACT_APP_DEV_MODE) {
       connectFunctionsEmulator(functions, "localhost", 5001);
@@ -45,6 +44,7 @@ function RegistrationForm({firebase}) {
     try {
       await registerContact({
         registrationRequest: formState,
+        recaptchaToken: token,
       });
       console.log("Successfully registered contact.");
     } catch (error) {
@@ -52,94 +52,95 @@ function RegistrationForm({firebase}) {
     }
   }
 
-  return (
-    <div>
-      <Form onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridGivenName">
-            <Form.Label>Given Name</Form.Label>
-            <Form.Control type="text" name="givenName" placeholder="Enter given name"
-              value={formState.givenName} onChange={handleChange}/>
-          </Form.Group>
+  window.handleSubmit = handleSubmit;
 
-          <Form.Group as={Col} controlId="formGridSurname">
-            <Form.Label>Surname</Form.Label>
-            <Form.Control type="text" name="surname" placeholder="Enter surname"
-              value={formState.surname} onChange={handleChange}/>
+  return (
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridGivenName">
+          <Form.Label>Given Name</Form.Label>
+          <Form.Control type="text" name="givenName" placeholder="Enter given name"
+            value={formState.givenName} onChange={handleChange}/>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridSurname">
+          <Form.Label>Surname</Form.Label>
+          <Form.Control type="text" name="surname" placeholder="Enter surname"
+            value={formState.surname} onChange={handleChange}/>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Col xs={4}>
+          <Form.Group as={Col} controlId="formGridAge">
+            <Form.Label>Age</Form.Label>
+            <Form.Select name="age" value={formState.age} onChange={handleChange}>
+              <option>Select your age group</option>
+              <option>18-22</option>
+              <option>23-25</option>
+              <option>26-30</option>
+              <option>31-40</option>
+              <option>41-60</option>
+              <option>60+</option>
+            </Form.Select>
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Col xs={4}>
-            <Form.Group as={Col} controlId="formGridAge">
-              <Form.Label>Age</Form.Label>
-              <Form.Select name="age" value={formState.age} onChange={handleChange}>
-                <option>Select your age group</option>
-                <option>18-22</option>
-                <option>23-25</option>
-                <option>26-30</option>
-                <option>31-40</option>
-                <option>41-60</option>
-                <option>60+</option>
-              </Form.Select>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridStreet">
-            <Form.Label>Street Name</Form.Label>
-            <Form.Control type="text" name="street" placeholder="Enter street name"
-              value={formState.street} onChange={handleChange}/>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridStreet">
+          <Form.Label>Street Name</Form.Label>
+          <Form.Control type="text" name="street" placeholder="Enter street name"
+            value={formState.street} onChange={handleChange}/>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Col xs={4}>
+          <Form.Group as={Col} controlId="formGridFlatNumber">
+            <Form.Label>Flat Number</Form.Label>
+            <Form.Control type="text" name="flatNumber" placeholder="Enter flat number"
+              value={formState.flatNumber} onChange={handleChange}/>
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Col xs={4}>
-            <Form.Group as={Col} controlId="formGridFlatNumber">
-              <Form.Label>Flat Number</Form.Label>
-              <Form.Control type="text" name="flatNumber" placeholder="Enter flat number"
-                value={formState.flatNumber} onChange={handleChange}/>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Col xs={4}>
-            <Form.Group as={Col} controlId="locality">
-              <Form.Label>Province</Form.Label>
-              <Form.Control ref={provinceElement} type="text" name="province" placeholder="Enter province"
-                onChange={handleChange}/>
-            </Form.Group>
-          </Col>
-          <Col xs={4}>
-            <Form.Group as={Col} controlId="sublocality_level_1">
-              <Form.Label>City/Village</Form.Label>
-              <Form.Control type="text" name="cityVillage" placeholder="Enter city of village"
-                value={formState.cityVillage} onChange={handleChange}/>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Col xs={4}>
-            <Form.Group as={Col} controlId="country">
-              <Form.Label>Country</Form.Label>
-              <Form.Control type="text" name="country" placeholder="Country"
-                value={formState.country} onChange={handleChange}/>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group className="mb-3" id="formGridAgeConfirmation">
-            <Form.Check type="checkbox" name="ageConfirmation" id="age-confirmation"
-              label="I am older than 18" value={formState.ageConfirmation} onChange={handleChange}/>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col xs={4}>
+          <Form.Group as={Col} controlId="locality">
+            <Form.Label>Province</Form.Label>
+            <Form.Control ref={provinceElement} type="text" name="province" placeholder="Enter province"
+              onChange={handleChange}/>
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Col xs={3}>
-            <Button variant="primary" type="submit">
+        </Col>
+        <Col xs={4}>
+          <Form.Group as={Col} controlId="sublocality_level_1">
+            <Form.Label>City/Village</Form.Label>
+            <Form.Control type="text" name="cityVillage" placeholder="Enter city of village"
+              value={formState.cityVillage} onChange={handleChange}/>
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col xs={4}>
+          <Form.Group as={Col} controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control type="text" name="country" placeholder="Country"
+              value={formState.country} onChange={handleChange}/>
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group className="mb-3" id="formGridAgeConfirmation">
+          <Form.Check type="checkbox" name="ageConfirmation" id="age-confirmation"
+            label="I am older than 18" value={formState.ageConfirmation} onChange={handleChange}/>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Col xs={3}>
+          <Button className="g-recaptcha" data-sitekey={process.env.REACT_APP_APP_CHECK_PUBLIC_KEY}
+            data-callback="handleSubmit" variant="primary" type="submit">
             Submit
-            </Button>
-          </Col>
-        </Row>
-      </Form>
-    </div>
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
